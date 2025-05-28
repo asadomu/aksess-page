00813f1219095afe59d1b748774cb02332bcbab8
asadomu <132898373+asadomu@users.noreply.github.com>
2025-05-28 15:14:45 +0700

    Delete Home.tsx

--- Home.tsx
+++
@@ -1,4 +0,0 @@
-// This file is deprecated and will be replaced by AksessLandingPage.tsx
-// Its content has been removed to avoid conflicts.
-// If AksessLandingPage.tsx is created as a new file, this old Home.tsx can be deleted.
-export {}; // Add an empty export to make it a module if needed by build tools
